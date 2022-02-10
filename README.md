c02a8d392d9dd325e81bf1b062ba7acf3f832974
Michael Smith <michael.smith@puppet.com>
2022-02-09 17:12:02 -0800

    Initial framework

---
+++ README.md
@@ -0,0 +1,17 @@
+# kubectl-run
+
+A Kubernetes addon for running an ephemeral container with a "mounted" volume.
+
+Example:
+```
+mkdir -p ./stuff
+echo "Test suite" > ./stuff/in.txt
+kubectl ran busybox -e VAR1=Hello -e VAR2=world -v ./stuff:/stuff -- sh -c 'cp /stuff/in.txt && echo "$VAR1 $VAR2" >> /stuff/out.txt'
+```
+
+It works by
+1. Starting a container with `sleep` in a new pod. You can optionally specify environment variables.
+2. Copies any "mounted" volumes into the container.
+3. Runs the specified command.
+4. Copies the "mounted" volumes back out of the container.
+5. Deletes the pod.
---
+++ kubectl-ran.go
@@ -0,0 +1,84 @@
+// A utility for updating a Kubernetes TLS secret if it has expired or any of
+// the inputs have changed.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+)
+
+var (
+	ranExample = `
+	# Start busybox and run a command
+	%[1]s ran busybox -- echo "Hello world"
+
+	# Run a command with environment variables
+	%[1]s ran busybox --env=VAR1=Hello --env=VAR2=world -- sh -c 'echo "$VAR1 $VAR2"'
+
+	# Run a command with a synced directory
+	%[1]s ran busybox --volume=./stuff:/stuff -- sh -c 'echo "Hello world" > /stuff/out.txt'
+`
+)
+
+type RanOptions struct {
+	configFlags *genericclioptions.ConfigFlags
+	genericclioptions.IOStreams
+	envVars []string
+}
+
+func NewRanOptions(streams genericclioptions.IOStreams) *RanOptions {
+	return &RanOptions{
+		configFlags: genericclioptions.NewConfigFlags(true),
+		IOStreams:   streams,
+	}
+}
+
+func (o *RanOptions) Validate() error {
+	return nil
+}
+
+func (o *RanOptions) Run() error {
+	return nil
+}
+
+func NewCmdRan(streams genericclioptions.IOStreams) *cobra.Command {
+	o := NewRanOptions(streams)
+
+	cmd := &cobra.Command{
+		Use:          `ran IMAGE [--env="key=value"] [--volume=src:dst] -- [COMMAND] [args...]`,
+		Short:        "Run a command in an ephemeral container with synced volume and environment.",
+		Example:      fmt.Sprintf(ranExample, "kubectl"),
+		SilenceUsage: true,
+		Args:         cobra.MinimumNArgs(2),
+		RunE: func(c *cobra.Command, args []string) error {
+			if err := o.Validate(); err != nil {
+				return err
+			}
+			if err := o.Run(); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringArrayVar(&o.envVars, "env", []string{}, "environment variables for the container")
+	o.configFlags.AddFlags(cmd.Flags())
+
+	return cmd
+}
+
+func main() {
+	flags := pflag.NewFlagSet("kubectl-ran", pflag.ExitOnError)
+	pflag.CommandLine = flags
+
+	root := NewCmdRan(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
+	if err := root.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
